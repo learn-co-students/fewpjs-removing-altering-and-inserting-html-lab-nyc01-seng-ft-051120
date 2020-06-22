373a20bdd1c267b7545e2e68eb06eae147b02cd7
callas4ever <thetseringnorbu@gmail.com>
2020-06-22 13:42:46 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// Write your code here!
+let main=document.getElementById('main')
+main.remove();
+let newHeader=document.createElement('h1');
+newHeader.id='victory'
+document.body.appendChild(newHeader);
+newHeader.innerHTML='YOUR-NAME is the champion'
